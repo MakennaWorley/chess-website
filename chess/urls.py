d11803183cabf60497149e2ff41cf039d07c4a22
Makenna Worley <makennaworley@gmail.com>
2024-09-22 21:18:02 -0600

    Added Logout button

--- chess/urls.py
+++ chess/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import login_view, signup_view, home_view
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('home/', home_view, name='home'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
